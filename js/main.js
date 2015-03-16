80170c7b7b1384b9000f4a69544556ece4d039fb
bluezebragames <bluezebragames@gmail.com>
2015-03-16 13:55:20 -0600

    First stuph!  Get hyped

---
+++ js/main.js
@@ -0,0 +1,36 @@
+var x = 0, ctx, repeat, move, counter = 0;
+
+var main = function () {
+    "use strict";
+    move = 0;
+    var canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
+    ctx.fillRect(0, 0, 50, 50);
+    //setTimeout(main, 10);
+};
+
+var toggle = function () {
+    "use strict";
+    if (move === 0) {move = .1; } else {move = 0; }
+    counter = x;
+    repeat();
+};
+
+repeat = function () {
+    "use strict";
+    counter += .1;
+    var p = document.getElementById("does this work");
+    p.innerHTML = move.toString() + " " + x.toString() + " " + counter.toString();
+    x += move;
+    
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillRect(x, 0, 50, 50);
+    if (x > ctx.canvas.width - 50) {
+        ctx.fillRect(x - ctx.canvas.width, 0, 50, 50);
+    }
+    if (x > ctx.canvas.width) {
+        x -= ctx.canvas.width;
+    }
+    setTimeout(repeat, 10);
+    
+};
